39193c9a44d0659c0c264b3a06c988d275378429
shuto410 <shuto@posl.ait.kyushu-u.ac.jp>
2019-10-24 14:37:31 +0900

    MapコンポーネントとSpotInfoコンポーネントを作成

---
+++ src/components/Map.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="map">
+    map
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Map extends Vue {
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/components/SpotInfo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="spot-info">
+      spot-info
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class SpotInfo extends Vue {
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
