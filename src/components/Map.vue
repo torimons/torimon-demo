a6be0db5e38cc6d54c854726e590b0d1f281e88d
imamura-gen <49475550+imamura-gen@users.noreply.github.com>
2019-11-01 12:57:45 +0900

    Update Map.vue
    ・マーカー表示
    　-表示メソッド
    　-表示に必要な情報を返すメソッド
    ・オブジェクト表示
    　-表示メソッド
    　-表示に必要な情報を返すメソッド
    ・表示マーカー切替メソッド
    ・表示オブジェクト切替メソッド
    ・マーカーを押した時に呼び出されるコールバック関数として使うメソッド

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -6,10 +6,69 @@
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import leaflet from 'leaflet/dist/leaflet.css';
+import L from leaflet;
+import store from './store'
+import { mapState } from 'vuex';
 
 @Component
 export default class Map extends Vue {
-}
+    map: any;
+    latlng: any;
+    object: any;
+
+    class Marker extends Map{
+        constructor(){
+            super(map);
+            super(latlng);
+            }
+        displyMarker(){
+            //マーカーの表示
+            L.marker(this.latlng).addTo(this.map);
+        }
+        returnMarkerInfo(latlng: number){
+            let loc = geoLocation();
+            //マーカーの情報を返す
+            if(location){
+                return this.$store.getters.getMarkerInfo;
+            }else{
+            //マーカーが無い場合の処理
+            }
+        }
+    }
+
+    function changeMarker(){
+        //マーカーの切り替え
+        let spot = $store.getters.getMarkerInfo;
+        if(spot){
+            map.removeLayer();
+            L.marker(latlng).addTo(map);
+        }else{
+            //スポットの情報が無い時の処理
+        }
+    }
+
+    class Object extends Map{
+        constructor(){
+            super(map);
+            super(object);
+        }
+        displayObject(){
+            //オブジェクトの表示
+        }
+        returnObjectInfo(){
+            //オブジェクトの情報を返す
+            return this.$store.getters.getObjectInfo;
+        }
+    }
+
+    function changeObject(){
+        //オブジェクトの切り替え
+    }
+
+    //マーカーに関するコールバック関数
+    function markerCallback(){
+    }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
