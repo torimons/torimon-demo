51b5173f23ee3f45ede3114463d69a0056cd7e88
shuto410 <shuto@posl.ait.kyushu-u.ac.jp>
2019-11-18 15:36:44 +0900

    アイコンの画像を設定して，ストアのスポットを表示するように

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -6,6 +6,7 @@
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import { mapViewStore } from '@/store/modules/MapViewModule';
 import { mapState } from 'vuex';
+import { SpotForMap } from '@/store/types'
 import store from '../store';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
@@ -51,12 +52,23 @@ export default class Map extends Vue {
         const centerPoint = bounds.getCenter();
         this.centerLat = centerPoint.x;
         this.centerLng = centerPoint.y;
-        
+        console.log("lat"+this.centerLat);
+        console.log("lng"+this.centerLng);
         this.map = L.map('map').setView([this.centerLat, this.centerLng], this.zoomLevel);
         this.tileLayer = L.tileLayer(
             'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
         ).addTo(this.map);
-        L.marker([this.centerLat, this.centerLng]).bindPopup('九大').addTo(this.map);
+        const sampleIcon = L.icon({
+            iconUrl: 'https://ke1q84.info/wp-content/uploads/2019/08/299BC8E1-F4BE-426C-A2D4-CC36C9D73AB5-500x400.jpeg',
+            iconRetinaUrl: 'https://ke1q84.info/wp-content/uploads/2019/08/299BC8E1-F4BE-426C-A2D4-CC36C9D73AB5-500x400.jpeg',
+            iconSize: [150, 120],
+            iconAnchor: [25, 50],
+            popupAnchor: [0, -50],
+        });
+        const spots: SpotForMap[] = mapViewStore.getSpotsForMap(mapViewStore.rootMapId);
+        spots.forEach(spot => {
+            L.marker([spot.coordinate.lat, spot.coordinate.lng], { icon: sampleIcon }).bindPopup(spot.name).addTo(this.map);
+        });
     }
 
     // ズームレベルや階層が変更された際のマーカー表示切り替え
