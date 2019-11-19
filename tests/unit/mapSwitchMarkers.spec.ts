7b27f62babdd2ad13802144babf20a540724f411
imamura-gen <49475550+imamura-gen@users.noreply.github.com>
2019-11-19 15:02:45 +0900

    テストの雛型作成
    Co-authored-by: sho25 <38647474+sho25@users.noreply.github.com>

--- tests/unit/mapSwitchMarkers.spec.ts
+++ tests/unit/mapSwitchMarkers.spec.ts
@@ -1,14 +1,29 @@
 import { mapViewStore } from '@/store/modules/MapViewModule';
 import { SpotForMap, Coordinate } from '@/store/types';
+import { shallowMount } from '@vue/test-utils';
 import Vue from 'vue';
 import Map from '@/components/Map.vue';
 
-describe('components/Map.vue', () => {
+describe('components/Map.vue マーカー切り替えのテスト', () => {
     beforeEach(() => {
         const event = new Event('testEvent');
     });
 
-    it('addMarkersのテスト', () => {
-        const map = new Vue(Map).$mount();
+    it('switchMarkerが呼ばれるとマーカーが切り替わる', () => {
+        const wrapper = shallowMount(Map, /*{
+            propsData: { propmap, ...} 
+        }*/);
+        console.log(wrapper);
+        //expect(wrapper.vm.markers).toBe([L.marker([wrapper.vm.centerLat, wrapper.vm.centerLng], { icon: this.testIcon })]);
+        // イベント発火
+        wrapper.trigger('zoomstart')
+        // 変更後の
+        const lat = mapViewStore.maps[0].spots[0].coordinate.lat
+        const lng = mapViewStore.maps[0].spots[0].coordinate.lng
+        //expect(wrapper.vm.markers).toBe([lat, lng])
+    });
+
+    it('addMarkers スポットの追加', () => {
+        
     });
 });
