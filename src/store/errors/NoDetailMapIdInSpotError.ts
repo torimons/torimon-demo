974c29f108750f1c8ca5f4ceae172c0a99574e1c
mirlosu <45028997+mirlosu@users.noreply.github.com>
2019-12-10 16:38:25 +0900

    エラーファイルの定義の追加及び分割
    Co-authored-by: Ken Matsui <kenchalros@gmail.com>

--- src/store/errors/NoDetailMapIdInSpotError.ts
+++ src/store/errors/NoDetailMapIdInSpotError.ts
@@ -1,9 +1,9 @@
-export class NoDetailMapError extends Error {
+export class NoDetailMapIdInSpotError extends Error {
     constructor(m?: string) {
         super(m);
         // Set the prototype explicitly.
         // (https://github.com/Microsoft/TypeScript-wiki/blob/
         // master/Breaking-Changes.md#extending-built-ins-like-error-array-and-map-may-no-longer-work)
-        Object.setPrototypeOf(this, NoDetailMapError.prototype);
+        Object.setPrototypeOf(this, NoDetailMapIdInSpotError.prototype);
     }
 }
