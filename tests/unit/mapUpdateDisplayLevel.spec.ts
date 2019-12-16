7a7b7da5568637bc0af0953dcc91cdec02f059e7
ikeda <ikeda@posl.ait.kyushu-u.ac.jp>
2019-12-16 15:20:40 +0900

    テスト作成

---
+++ tests/unit/mapUpdateDisplayLevel.spec.ts
@@ -0,0 +1,53 @@
+import { mapViewStore } from '@/store/modules/MapViewModule';
+import { MapViewState, SpotForMap, Coordinate } from '@/store/types';
+import { shallowMount } from '@vue/test-utils';
+import Vue from 'vue';
+import Map from '@/components/Map.vue';
+import 'leaflet/dist/leaflet.css';
+import L, { map } from 'leaflet';
+import { cloneDeep } from 'lodash';
+import { testMapViewState } from '../resources/testMapViewState';
+
+const mapViewStoreTestData: MapViewState = cloneDeep(testMapViewState);
+
+/** モック用の関数，paramに与えた値を返す関数を返す
+ * @param n 返して欲しい値
+ * @return nを返す関数
+ */
+function setReturnOfGetZoom(n: number): (() => {}) {
+    return  (jest.fn(() => {
+        return n;
+    }));
+}
+
+describe('components/Map.vue zoomlevel切り替えのテスト', () => {
+    let wrapper: any;
+
+    beforeEach(() => {
+        mapViewStore.setMapViewState(mapViewStoreTestData);
+        wrapper = shallowMount(Map, {
+            attachToDocument: true,
+        });
+    });
+
+    afterEach(() => {
+        // Map components already initialized防止
+        wrapper.destroy();
+    });
+
+    it('zoomlevelの値によってstateのdisplayLevelを変更する', () => {
+        // getZoom()のモック
+        wrapper.vm.map.getZoom = setReturnOfGetZoom(18);
+        // 閾値(19)未満の場合，displayLevelはdefault
+        wrapper.vm.updateDisplayLevel();
+        const currentLargeDisplayLevel = mapViewStore.getDisplayLevel();
+        expect(currentLargeDisplayLevel).toBe('default');
+
+        // getZoom()のモック
+        // 閾値(19)以上の場合，displayLevelはdetail
+        wrapper.vm.map.getZoom = setReturnOfGetZoom(19);
+        wrapper.vm.updateDisplayLevel();
+        const currentSmallDisplayLevel = mapViewStore.getDisplayLevel();
+        expect(currentSmallDisplayLevel).toBe('detail');
+    });
+});
