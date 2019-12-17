c94d044af100bdeceee02eb2ebf5119f028f8a39
matsui <matsui@posl.ait.kyushu-u.ac.jp>
2019-12-17 18:25:05 +0900

    Vuetifyコンポーネントを含んだテスト実行時エラーの修正

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
     preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
     moduleNameMapper: {
         "\\.(css|scss)$": "<rootDir>/node_modules/jest-css-modules"
-    }
+    },
+    setupFilesAfterEnv: ['./tests/setup.ts']
 }
