d8a5afb3d69f2324ca718e09b3caa5bec72f46e9
ikeda <ikeda@posl.ait.kyushu-u.ac.jp>
2019-12-17 16:17:07 +0900

    DefaultSpotクラス作成

---
+++ src/DefaultSpot.ts
@@ -0,0 +1,25 @@
+import L, {Marker, LatLngExpression} from 'leaflet';
+
+export default class DefaulSpotMarker extends L.Marker {
+    private icon: L.Icon = L.icon({
+        iconUrl: 'https://github.com/torimons/torimon/blob/master/public/leaflet/icons/marker-icon-2x.png?raw=true',
+        iconSize: [50, 82],
+        iconAnchor: [25, 80],
+        popupAnchor: [-3, -76],
+        shadowSize: [68, 95],
+        shadowAnchor: [22, 94],
+    });
+
+    constructor(latlng: LatLngExpression) {
+        super(latlng);
+        this.setIcon(this.icon);
+    }
+
+    public addTo(map: L.Map | L.LayerGroup<any>): this {
+        return super.addTo(map).on('click', this.callback);
+    }
+
+    private callback(): void {
+        // updateFocusedMarker
+    }
+}
