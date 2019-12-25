671ff2d85881911bd2dd8e1fd24a869dcc972ebf
shuto410 <shuto@posl.ait.kyushu-u.ac.jp>
2019-12-25 20:51:56 +0900

    vuexのstateをmoduleから分離

--- src/store/modules/MapViewModule.ts
+++ src/store/modules/MapViewModule.ts
@@ -1,56 +1,12 @@
 import { Getters, Mutations, Actions, Module } from 'vuex-smart-module';
+import MapViewState from './MapViewState';
 import { Map, Spot, SpotInfo, SpotForMap, Bounds, DisplayLevelType, Coordinate, Node } from '@/store/types';
-import { sampleMaps } from '@/store/modules/sampleMaps';
 import { NoDetailMapsError } from '../errors/NoDetailMapsError';
 import { NoDetailMapIdInSpotError } from '../errors/NoDetailMapIdInSpotError';
 import { MapNotFoundError } from '../errors/MapNotFoundError';
 import { SpotNotFoundError } from '../errors/SpotNotFoundError';
 import { mapViewGetters } from '@/store';
 
-export class MapViewState {
-    /**
-     * 複数のマップの情報を持つ
-     * - 大元の地図と各スポットの持つ
-     *   詳細地図(ピンチインで出てくるやつ)など含む
-     * - 単体テスト以外の目視テスト等のために
-     *   外部モジュールのsampleMapsで初期化
-     * 将来的にはvuexのmutationで登録する
-     */
-    public maps: Map[] = sampleMaps;
-
-    /*
-     * 大元の親のMapのID
-     */
-    public rootMapId: number = 0;
-
-    /**
-     * Mapコンポーネントで選択されているMap，およびスポットのID
-     */
-    public focusedSpot: {mapId: number, spotId: number} = {
-        mapId: 0,
-        spotId: 0,
-    };
-
-    /**
-     * SpotInfoコンポーネントの表示非表示状態を保持
-     */
-    public spotInfoIsVisible: boolean = false;
-
-    /**
-     * - 画面上で表示されている
-     * - 半径〇〇内で最も画面中央に近い
-     * - 詳細マップを持っている
-     * スポットのIDを保持する変数
-     * 条件に当てはまるスポットがない場合nullを持つ
-     */
-    public idOfCenterSpotInRootMap: number | null = null;
-
-    /**
-     * ズームレベルに応じて切り替わる表示レベルを保持
-     */
-    public displayLevel: DisplayLevelType = 'default';
-
-}
 
 export class MapViewGetters extends Getters<MapViewState> {
     get maps(): Map[] {
