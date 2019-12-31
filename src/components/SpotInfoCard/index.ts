db01112e377e3b7cb1dacd80a2eb6e4eb861d949
matsui <matsui@posl.ait.kyushu-u.ac.jp>
2019-12-31 23:50:20 +0900

    watchの変更&interfaceの継承を追加

--- src/components/SpotInfoCard/index.ts
+++ src/components/SpotInfoCard/index.ts
@@ -1,52 +1,52 @@
 import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
-import { store, mapViewGetters, mapViewMutations } from '@/store';
-import { Spot, SpotInfo } from '@/store/types';
+import { store, mapViewGetters } from '@/store';
+import { MapViewGetters } from '@/store/modules/MapViewModule/MapViewGetters';
+import { SpotInfo } from '@/store/types';
 
 @Component
 /**
  * マップ上でスポットを選択した際に表示されるコンポーネント．
  * vuex上のstateを見て，表示内容，および表示/非表示を自動的に切り替える．
  */
-export default class SpotInfoCard extends Vue {
+export default class SpotInfoCard extends Vue implements SpotInfo {
+
+    // SpotInfoインターフェースのメンバ
+    public name: string = '';
+    public description: string = '';
+    public attachment: [{name: string, url: string}] = [{name: '', url: ''}];
 
     private isVisible: boolean = false;
-    private spotName: string = '';
-    private description: string = '';
 
-    /**
-     * updateSpotInfoContentで監視するプロパティ
-     * @return 選択されているスポット
-     */
-    get focusedSpot(): {mapId: number, spotId: number} {
-        return mapViewGetters.focusedSpot;
+    public mounted() {
+        // focusedSpotの切り替えを検知して表示する内容を更新
+        store.watch(
+            (state, getters: MapViewGetters) => getters.focusedSpot,
+            (value, oldValue) => this.updateSpotInfoContent(value),
+        );
+        // spotInfoIsVisibleの切り替えを検知して表示/非表示状態を更新
+        store.watch(
+            (state, getters: MapViewGetters) => getters.spotInfoIsVisible,
+            (value, oldValue) => this.updateSpotInfoIsVisible(value),
+        );
     }
 
     /**
      * 選択されているスポットIDの変更を検知すると，spotName, othersを更新して表示内容を更新する．
+     * @param newFocusedSpot 変更後のマップIdとスポットId
      */
-    @Watch('focusedSpot')
     private updateSpotInfoContent(
         newFocusedSpot: {mapId: number, spotId: number},
-        oldFocusedSpot: {mapId: number, spotId: number},
     ): void {
         const spotInfo: SpotInfo = mapViewGetters.getSpotInfo(newFocusedSpot);
-        this.spotName = spotInfo.name;
+        this.name = spotInfo.name;
         this.description = spotInfo.description;
     }
 
-    /**s
-     * updateSpotInfoIsVisibleで監視するプロパティ
-     * @return SpotInfoの可視化状態
-     */
-    get spotInfoIsVisible(): boolean {
-        return mapViewGetters.spotInfoIsVisible;
-    }
-
     /**
-     * コンポーネントの表示/非表示を，visibleを更新して切り替える.．
+     * コンポーネントの表示/非表示を，visibleを更新する．
+     * @param newVisibleState コンポーネントの可視化状態
      */
-    @Watch('spotInfoIsVisible')
-    private updateSpotInfoIsVisible(newVisibleState: boolean, oldVisibileState: boolean): void {
+    private updateSpotInfoIsVisible(newVisibleState: boolean): void {
         this.isVisible = newVisibleState;
     }
 
--- src/components/SpotInfoCard/index.vue
+++ src/components/SpotInfoCard/index.vue
@@ -9,7 +9,7 @@
                 >
                 <v-list-item two-line>
                     <v-list-item-content>
-                        <v-list-item-title class="headline">{{ spotName }}</v-list-item-title>
+                        <v-list-item-title class="headline">{{ name }}</v-list-item-title>
                         <v-list-item-subtitle>{{ description }}</v-list-item-subtitle>
                     </v-list-item-content>
                 </v-list-item>
