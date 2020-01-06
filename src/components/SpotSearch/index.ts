c82b7e04cbcf7c929b06d9a0a243e3a145210b36
ikeda <ikeda@posl.ait.kyushu-u.ac.jp>
2020-01-06 13:44:41 +0900

    スポットの取得部分をチェーンに変更

--- src/components/SpotSearch/index.ts
+++ src/components/SpotSearch/index.ts
@@ -20,8 +20,9 @@ export default class SpotSearch extends Vue {
 
     public mounted() {
         // 全てのマップからスポットを取得，一つの配列に結合する
-        const spotsOfMaps: Spot[][] = mapViewGetters.maps.map((map: Map) => map.spots);
-        this.targetSpots = Array.prototype.concat.apply([], spotsOfMaps);
+        mapViewGetters.maps
+            .map((map: Map) => map.spots)
+            .forEach((spots) => this.targetSpots = this.targetSpots.concat(spots));
         // 上で取得したspotを検索対象にセットしたSearchクラスのインスタンス作成
         this.search = new Search(this.targetSpots);
     }
