085326c6de39f5b5f1387810815e8b1a4ff349c0
ikeda <ikeda@posl.ait.kyushu-u.ac.jp>
2020-01-09 21:09:26 +0900

    spotSearchのテスト追加

--- tests/unit/components/SpotSearch/spotSearch.spec.ts
+++ tests/unit/components/SpotSearch/spotSearch.spec.ts
@@ -1,5 +1,6 @@
 import { shallowMount } from '@vue/test-utils';
 import SpotSearch from '@/components/SpotSearch/index.vue';
+import SearchBox from '@/components/SearchBox/index.vue';
 
 describe('SpotSearchコンポーネントのテスト', () => {
     let wrapper: any;
@@ -11,13 +12,16 @@ describe('SpotSearchコンポーネントのテスト', () => {
     });
 
     afterEach(() => {
-        // Map components already initialized防止
         wrapper.destroy();
     });
 
     it('toggleSpotListイベントが発火するとsetSpotListIsVisibleが呼ばれる', () => {
-        // イベント発火
-
-        // 関数呼び出し確認
+        // これは単体テストなのか...?
+        // 初期値はfalse
+        expect(wrapper.vm.spotListIsVisible).toBe(false);
+        // 子コンポーネントからのemit
+        wrapper.find(SearchBox).vm.$emit('toggleSpotList', true);
+        // trueに変わっていることを確認
+        expect(wrapper.vm.spotListIsVisible).toBe(true);
     });
 });
