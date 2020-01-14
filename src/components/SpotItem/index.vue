0734bee297dfe425ea9cca922c883b927ed129bf
imamura-gen <igen.kuel@gmail.com>
2020-01-14 15:27:22 +0900

    コンフリクト解消と一部修正

--- src/components/SpotItem/index.vue
+++ src/components/SpotItem/index.vue
@@ -1,24 +1,17 @@
 <template>
 	<div id="spot-item">
-<<<<<<< HEAD
-        <v-btn
-            @click="updateFocusedSpot()"
-        >
-        {{ spotName }}
-        {{ distance }}
-        </v-btn>
-=======
         <v-card
             tile
         >
             <v-list-item two-line>
                 <v-list-item-content>
-                    <v-list-item-title class="headline">{{ spotName }}</v-list-item-title>
-                    <v-list-item-subtitle>{{ distance }}</v-list-item-subtitle>
+                    <v-btn @click="updateFocusedSpot()">
+                        <v-list-item-title class="headline">{{ spotName }}</v-list-item-title>
+                        <v-list-item-subtitle>{{ distance }}</v-list-item-subtitle>
+                    </v-btn>
                 </v-list-item-content>
             </v-list-item>
         </v-card>
->>>>>>> 338c5cb405f38f50fb0fbbabc3cf9e17486f5b3e
 	</div>
 </template>
 
