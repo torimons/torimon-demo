bedf67420f0fb98498ed89ef58811c721bc0f841
ikeda <ikeda@posl.ait.kyushu-u.ac.jp>
2020-01-20 16:51:58 +0900

    テストの修正，spotNameを引数に追加

--- tests/unit/components/Map/defaultSpotMarker.spec.ts
+++ tests/unit/components/Map/defaultSpotMarker.spec.ts
@@ -9,20 +9,21 @@ describe('DefaultSpotMarkers', () => {
         const testLng = 0;
         const testMapId = 0;
         const testSpotId = 0;
-        const spotMarker = new DefaultSpotMarker([testLat, testLng], testMapId, testSpotId);
+        const testSpotName = 'testSpot';
+        const spotMarker = new DefaultSpotMarker([testLat, testLng], testSpotName, testMapId, testSpotId);
         const expectedMarker = L.marker([testLat, testLng]);
         expect(spotMarker.getLatLng()).toStrictEqual(expectedMarker.getLatLng());
     });
 
     it('updateFocusedMarkerがmapIdとspotIdをfocusedSpotにsetする', () => {
-        const testMarker = new DefaultSpotMarker([0, 0], 0, 1);
+        const testMarker = new DefaultSpotMarker([0, 0], 'hoge', 0, 1);
         (testMarker as any).updateFocusedMarker();
         const expectedFocusedMarker: {mapId: number, spotId: number} = {mapId: 0, spotId: 1};
         expect(mapViewGetters.focusedSpot).toStrictEqual(expectedFocusedMarker);
     });
 
     it('updateFocusedMarkerがspotInfoIsVisibleをtrueに変更する', () => {
-        const testMarker = new DefaultSpotMarker([0, 0], 0, 0);
+        const testMarker = new DefaultSpotMarker([0, 0], 'hoge', 0, 0);
         (testMarker as any).updateFocusedMarker();
         expect(mapViewGetters.spotInfoIsVisible).toBe(true);
     });
