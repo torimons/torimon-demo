6dbe95dda25f3efab3df1d28f78611fd71286e43
ikeda <ikeda@posl.ait.kyushu-u.ac.jp>
2020-01-21 11:32:35 +0900

    fontSizeなどをメンバから削除，不要なimportなど削除，改行等のフォーマット

--- src/components/Map/Marker/DefaultSpotMarker.ts
+++ src/components/Map/Marker/DefaultSpotMarker.ts
@@ -1,6 +1,5 @@
-import L, {Marker, LatLngExpression, LeafletEvent} from 'leaflet';
+import L, {LatLngExpression} from 'leaflet';
 import { mapViewMutations } from '@/store';
-import { MapViewState } from '@/store/modules/MapViewModule/MapViewState';
 
 export default class DefaultSpotMarker extends L.Marker {
     private mapId: number;
@@ -8,16 +7,12 @@ export default class DefaultSpotMarker extends L.Marker {
     private spotName!: string;
     private normalColor: string = '#3F8373';
     private selectedColor: string = '#AE56B3';
-    private nameLabelIcon!: L.DivIcon;
     private nameLabelMarker!: L.Marker;
     private icon: L.DivIcon = L.divIcon({
         className: 'custom-div-icon',
         html: `<div class="marker-pin"></div><i class="material-icons" style="font-size:48px; color:${this.normalColor};">room</i>`,
         iconAnchor: [24, 50],
     });
-    private isSelected: boolean = false;
-    private fontSize: number = 14;
-    private widthRate: number = this.fontSize * 5 / 3;
 
     constructor(latlng: LatLngExpression, spotName: string, mapId: number, spotId: number) {
         super(latlng);
@@ -43,13 +38,17 @@ export default class DefaultSpotMarker extends L.Marker {
      * @param latlng スポットの座標
      */
     private createNameLabelMarker(latlng: LatLngExpression): void {
+        const fontSize: number = 14;
+        const widthRate: number = fontSize * 5 / 3;
         // だいたいfont-size:12のときwidthはString.length * 15くらいがちょうどいい
         const htmlTemplate =
-            `<div style="width:${Math.round(this.spotName.length * this.widthRate)}px;font-size:${this.fontSize}px;">${this.spotName}</div>`;
+            `<div
+                style="width:${Math.round(this.spotName.length * widthRate)}px;font-size:${fontSize}px;"
+            >${this.spotName}</div>`;
         const nameLabelIcon = L.divIcon({
             className: 'name-label',
             html: htmlTemplate,
-            iconAnchor: [(this.spotName.length - 1) * this.widthRate / 3, 0],
+            iconAnchor: [(this.spotName.length - 1) * widthRate / 3, 0],
         });
         this.nameLabelMarker = L.marker(latlng, {icon: nameLabelIcon});
     }
@@ -67,7 +66,6 @@ export default class DefaultSpotMarker extends L.Marker {
      * @param isSelected true/false
      */
     private setSelected(isSelected: boolean): void {
-        this.isSelected = isSelected;
         const color = isSelected ? this.selectedColor : this.normalColor;
         const htmlTemplate =
             `<div class="marker-pin"></div><i class="material-icons" style="font-size:48px; color:${color};">room</i>`;
