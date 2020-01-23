07e089a6a6ae87543c8a48688aa6d76d6c2d379e
matsui <matsui@posl.ait.kyushu-u.ac.jp>
2020-01-23 14:05:20 +0900

    複数ワードによるOR検索機能の追加，スポットの名前以外を検索対象に追加
    - parentSpotNameとdescriptionを検索対象にしました．

--- src/utils/Search.ts
+++ src/utils/Search.ts
@@ -14,13 +14,59 @@ export default class Search {
      * @return keywordにかかったスポットのリスト
      */
     public searchSpots(keyword: string): Spot[] {
-        if (keyword === '') {
+        if (keyword === '' || keyword === null) {
             return [];
         }
-        const searchExp = new RegExp(keyword, 'i'); // 'i'オプションで大文字小文字を区別しない
-        return this.targetSpots.filter((s: Spot) => {
-            return s.name.match(searchExp);
-        });
+        const keywords: string[][] = keyword.split(/\s+/)
+            .filter((word: string) => word !== '')
+            .map((word: string) => [word]);
+        const keywordsRegExp = this.compile(keywords);
+        return this.targetSpots.filter((s: Spot) => this.spotIsMatchToKeywords(s, keywordsRegExp));
+    }
+
+    /**
+     * 検索単語の配列を受け取り，前処理した正規表現オブジェクトとして返す.
+     * [
+     *  ['a', 'b', 'c'],
+     *  ['d'],
+     *  ['e', 'f'],
+     * ]
+     * のとき，検索条件は ('a' and 'b' and 'c') or ('d') or ('e' and 'f')
+     * @param cond 検索条件の配列
+     * @return 検索条件を表した正規表現オブジェクト
+     */
+    private compile(cond: string[][]) {
+        const joinAnd = (arr: string[]): string => {
+            return '^(?=[\\s\\S]*' + arr.join(')(?=[\\s\\S]*') + ')';
+        };
+        const joinOr = (arr: string[]): string => {
+            return '(?:' + arr.join('|') + ')';
+        };
+        const escape = (str: string): string => {
+            return str.replace(/(?=[(){}\[\].*\\^$?])/, '\\');
+        };
+        const rx = joinOr(cond.map((inner: string[]): string => {
+            return joinAnd(inner.map(escape));
+        }));
+        const regExp: string = rx.replace(/=\[\\s\\S\]\*-/g, '![\\s\\S]*');
+        return new RegExp(regExp);
+    }
+
+    /**
+     * スポットが正規表現にマッチするかどうかを判定する
+     * @param spot filter対象のスポット
+     * @param keywordsRegExp 検索キーワードの正規表現オブジェクト
+     * @return isMatch スポットが正規表現にマッチした場合true, マッチしなければfalse
+     */
+    private spotIsMatchToKeywords(spot: Spot, keywordsRegExp: RegExp): boolean {
+        let isMatch: boolean = false;
+        isMatch = isMatch || spot.name.match(keywordsRegExp) !== null;
+        if (spot.parentSpotName !== undefined) {
+            isMatch = isMatch || (spot.parentSpotName.match(keywordsRegExp) !== null);
+        }
+        if (spot.description !== undefined) {
+            isMatch = isMatch || (spot.description.match(keywordsRegExp) !== null);
+        }
+        return isMatch;
     }
 }
-
