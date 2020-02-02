8779ce34cfe0ec9d9a769b2c6af8c473fa131e08
ikeda <ikeda@posl.ait.kyushu-u.ac.jp>
2020-02-02 16:43:24 +0900

    メンバ宣言省略

--- src/Map/Map.ts
+++ src/Map/Map.ts
@@ -2,20 +2,10 @@ import Spot from '@/Spot/Spot.ts';
 import { Bounds } from '@/store/types';
 
 export default class TestMap {
-    // 必須
-    private id!: number;
-    private name!: string;
-    private spots!: Spot[];
-    private bounds!: Bounds;
-    // オプジョナル
-    private parentSpot?: Spot;
-
-    // parentSpotはオプション引数
-    constructor(id: number, name: string, spots: Spot[], bounds: Bounds, parentSpot?: Spot) {
-        this.id = id;
-        this.name = name;
-        this.bounds = bounds;
-        this.spots = spots;
-        this.parentSpot = parentSpot;
+    constructor(private id: number,
+                private name: string,
+                private spots: Spot[],
+                private bounds: Bounds,
+                private parentSpot?: Spot) {
     }
 }
--- src/Spot/Spot.ts
+++ src/Spot/Spot.ts
@@ -2,34 +2,14 @@ import { Coordinate, Shape } from '@/store/types.ts';
 import Map from '@/Map/Map.ts';
 
 export default class Spot {
-    // 必須
-    private id!: number;
-    private name!: string;
-    private coordinate!: Coordinate;
-    private parentMap!: Map;
-    // オプジョナル
-    private shape?: Shape;
-    private floorName?: string;
-    private detailMapLevelNames?: string[];
-    private description?: string;
-    private attachment?: [
-        {
-            name: string,
-            url: string,
-        }
-    ];
-
-    constructor(id: number, name: string, coordinate: Coordinate, parentMap: Map,
-                shape?: Shape, floorName?: string, detailMapLevelNames?: string[],
-                description?: string, attachment?: [{name: string, url: string}]) {
-        this.id = id;
-        this.name = name;
-        this.coordinate = coordinate;
-        this.parentMap = parentMap;
-        this.shape = shape;
-        this.floorName = floorName;
-        this.detailMapLevelNames = detailMapLevelNames;
-        this.description = description;
-        this.attachment = attachment;
+    constructor(private id: number,
+                private name: string,
+                private coordinate: Coordinate,
+                private parentMap: Map,
+                private shape?: Shape,
+                private floorName?: string,
+                private detailMapLevelNames?: string[],
+                private description?: string,
+                private attachment?: [{name: string, url: string}]) {
     }
 }
