b95fb43d41ac1d37d2e2b442af750e80eaf0577e
ikeda <ikeda@posl.ait.kyushu-u.ac.jp>
2020-02-13 14:35:04 +0900

    findDescendantSpot追加

--- src/Map/Map.ts
+++ src/Map/Map.ts
@@ -9,4 +9,25 @@ export default class Map {
                 private parentSpot?: Spot,
                 private floorName?: string) {
     }
+
+    /**
+     * 指定したidをもつ子孫スポットを探す
+     * @params id 指定するid
+     * @returns 該当するスポット，またはnull
+     */
+    public findDescendantSpot(id: number): Spot | null {
+        for (const spot of this.spots) {
+            // 自身の子が該当するかチェック
+            if (spot.getId() === id) {
+                return spot;
+            }
+            // 子孫が該当するかチェック
+            const foundSpot: Spot | null = spot.findDescendantSpot(id);
+            if (foundSpot === null) {
+                continue;
+            }
+            return foundSpot;
+        }
+        return null;
+    }
 }
--- src/Spot/Spot.ts
+++ src/Spot/Spot.ts
@@ -12,4 +12,29 @@ export default class Spot {
                 private description?: string,
                 private attachment?: [{name: string, url: string}]) {
     }
+
+    /**
+     * 自身のidを返す
+     * @returns id 自身のid
+     */
+    public getId(): number {
+        return this.id;
+    }
+
+    /**
+     * 指定したidをもつ子孫スポットを探す
+     * @params id 指定するid
+     * @returns 該当するスポット，またはnull
+     */
+    public findDescendantSpot(id: number): Spot | null {
+        // 自身の子マップの子孫を探す
+        for (const map of this.detailMaps) {
+            const foundSpot: Spot | null = map.findDescendantSpot(id);
+            if (foundSpot === null) {
+                continue;
+            }
+            return foundSpot;
+        }
+        return null;
+    }
 }
