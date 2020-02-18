644673461d5131da83c82ef9a6f3d4c3ce556230
ikeda <ikeda@posl.ait.kyushu-u.ac.jp>
2020-02-18 16:44:57 +0900

    親・子を追加するメソッド作成

--- src/Map/Map.ts
+++ src/Map/Map.ts
@@ -2,11 +2,33 @@ import Spot from '@/Spot/Spot.ts';
 import { Bounds } from '@/store/types';
 
 export default class Map {
+    private parentSpot: Spot | undefined = undefined;
+    private spots: Spot[] = [];
+
     constructor(private id: number,
                 private name: string,
-                private spots: Spot[],
                 private bounds: Bounds,
-                private parentSpot?: Spot,
                 private floorName?: string) {
+    }
+
+    /**
+     * 親スポットを登録する
+     * @params 登録する親スポット
+     * @errors 親スポットがすでに登録されている場合エラーを送出する
+     */
+    public registerParentSpot(parentSpot: Spot) {
+        if (this.parentSpot === undefined) {
+            this.parentSpot = parentSpot;
+        } else {
+            throw new Error('ParentSpot is already registered');
+        }
+    }
+
+    /**
+     * スポットを追加する
+     * @params 追加するスポット
+     */
+    public appendSpots(spots: Spot[]) {
+        spots.map((spot) => this.spots.push(spot));
     }
 }
--- src/Spot/Spot.ts
+++ src/Spot/Spot.ts
@@ -2,14 +2,36 @@ import { Coordinate, Shape } from '@/store/types.ts';
 import Map from '@/Map/Map.ts';
 
 export default class Spot {
+    private parentMap: Map | undefined = undefined;
+    private detailMaps: Map[] = [];
+
     constructor(private id: number,
                 private name: string,
                 private coordinate: Coordinate,
-                private parentMap: Map,
-                private detailMaps: Map[],
                 private shape?: Shape,
                 private floorName?: string,
                 private description?: string,
                 private attachment?: [{name: string, url: string}]) {
+    }
+
+    /**
+     * 親マップを登録する
+     * @params 登録する子スポット
+     * @errors 親マップがすでに登録されている場合エラーを送出する
+     */
+    public registerParentMap(parentMap: Map) {
+        if (this.parentMap === undefined) {
+            this.parentMap = parentMap;
+        } else {
+            throw new Error('ParentMap is already registered');
+        }
+    }
+
+    /**
+     * 詳細マップを追加する
+     * @params 追加する詳細マップの配列
+     */
+    public appendDetailMaps(detailMaps: Map[]) {
+        detailMaps.map((map) => this.detailMaps.push(map));
     }
 }
