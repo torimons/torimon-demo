81eb48020c154d8d0ed8fe18cced790c49a8cf42
ikeda <ikeda@posl.ait.kyushu-u.ac.jp>
2020-03-09 17:58:23 +0900

    MapクラスfindDescendantSpotのテスト追加

--- tests/unit/Map/mapFindDescendant.spec.ts
+++ tests/unit/Map/mapFindDescendant.spec.ts
@@ -7,15 +7,29 @@ describe('Mapクラス，findDescendantSpot', () => {
         topL: {lat: 0, lng: 0},
         botR: {lat: 0, lng: 0},
     };
+    const testCoord = { lat: 0, lng: 0 };
 
     it('子スポットがないor見つからない場合にnullを返す', () => {
-        map = new Map(0, 'testMap', [], testBounds, undefined, undefined);
-        expect(map.findDescendantSpot(0)).toBe(null);
+        const searchId = 0;
+        // 子スポットなしの時
+        map = new Map(0, 'testMap', testBounds);
+        expect(map.findDescendantSpot(searchId)).toBe(null);
+
+        // 見つからない場合
+        const notTargetId = 999;
+        const notTargetSpot = new Spot(notTargetId, 'notTargetSpot', testCoord);
+        map.addSpots([notTargetSpot]);
+        expect(map.findDescendantSpot(searchId)).toBe(null);
     });
 
-    it('子スポットに存在する場合にそのスポットを返す', () => {
+    it('検索対象スポットが子スポットに存在する場合にそのスポットを返す', () => {
         const targetId = 0;
-        const targetSpot = new Spot(targetId, 'targetSpot', testBounds, )
+        map = new Map(0, 'testMap', testBounds);
+        // 検索したいスポット生成，登録
+        const targetSpot = new Spot(targetId, 'targetSpot', testCoord);
+        map.addSpots([targetSpot]);
+
+        expect(map.findDescendantSpot(targetId)).toBe(targetSpot);
     });
 });
 
