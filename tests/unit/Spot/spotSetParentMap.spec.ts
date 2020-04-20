40beb93a3b9f47706c4a19fa9dc786e00ae64078
matsui <matsui@posl.ait.kyushu-u.ac.jp>
2020-04-20 17:39:09 +0900

    Map、Spotのadd処理内でset処理を、set処理内でadd処理を呼び出すように修正

--- tests/unit/Spot/spotSetParentMap.spec.ts
+++ tests/unit/Spot/spotSetParentMap.spec.ts
@@ -16,4 +16,32 @@ describe('Spotクラスの親マップ登録のテスト', () => {
         spot.setParentMap(testMap);
         expect((spot as any).parentMap).toBe(testMap);
     });
+
+    it('異なるparentMapの登録は禁止する', () => {
+        const testMap: Map = new Map(0, 'testMap', testBounds, undefined);
+        const anotherMap: Map = new Map(1, 'testMap', testBounds, undefined);
+        const testSpot = new Spot(0, 'testSpot', testCoord, undefined, undefined, undefined);
+        // 登録
+        testSpot.setParentMap(testMap);
+        testSpot.setParentMap(anotherMap);
+        // 最初に登録されたparentSpotが登録されたままになる
+        expect((testSpot as any).parentMap).toStrictEqual(testMap);
+    });
+
+    it('parentMap登録時に、parentMapの子spotとして自身を登録する', () => {
+        const testMap: Map = new Map(0, 'testMap', testBounds, undefined);
+        const testSpot = new Spot(0, 'testSpot', testCoord, undefined, undefined, undefined);
+        testSpot.setParentMap(testMap);
+        expect((testMap as any).spots.includes(testSpot)).toBe(true);
+    });
+
+    it('親マップが登録済みかどうかをhasParentMapにより判定する', () => {
+        const testMap: Map = new Map(0, 'testMap', testBounds, undefined);
+        const testSpot: Spot = new Spot(0, 'testSpot', testCoord, undefined, undefined, undefined, undefined);
+        // 登録前の判定
+        expect(testSpot.hasParentMap()).toBe(false);
+        // 登録後の判定
+        (testSpot as any).parentMap = testMap;
+        expect(testSpot.hasParentMap()).toBe(true);
+    });
 });
