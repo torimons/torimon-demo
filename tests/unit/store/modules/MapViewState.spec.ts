b1b00d62a04aed9ff8dbfec4aacfe1a92c439d5c
mirlosu <45028997+mirlosu@users.noreply.github.com>
2020-04-30 17:41:39 +0900

    テストコメントを追加

--- tests/unit/store/modules/MapViewState.spec.ts
+++ tests/unit/store/modules/MapViewState.spec.ts
@@ -9,11 +9,9 @@ describe('MapViewState.tsのテスト', () => {
         const testRawMap = testMapViewState3.maps;
         const testRawMapData = testRawMap[0];
         const testMapInstance = createMapInstance(testRawMapData);
-        const expectMapProperty = ['id', 'name', 'bounds', 'floorName', 'parentSpot', 'spots'];
         const expectMapId = testRawMapData.id;
         const expectMapName = testRawMapData.name;
         const expectMapBounds = testRawMapData.bounds;
-        expect(Object.keys(testMapInstance)).toStrictEqual(expectMapProperty);
         expect((testMapInstance as any).id).toStrictEqual(expectMapId);
         expect((testMapInstance as any).name).toStrictEqual(expectMapName);
         expect((testMapInstance as any).bounds).toStrictEqual(expectMapBounds);
@@ -22,11 +20,9 @@ describe('MapViewState.tsのテスト', () => {
         const testRawMap = testMapViewState3.maps;
         const testRawMapData = testRawMap[0];
         const testSpotInstance = createSpotInstance(testRawMapData.spots[0]);
-        const expectSpotProperty = ['id', 'name', 'coordinate', 'shape', 'floorName', 'description', 'attachment', 'parentMap', 'detailMaps'];
         const expectSpotId = testRawMapData.spots[0].id;
         const expectSpotName = testRawMapData.spots[0].name;
         const expectSpotCoordinate = testRawMapData.spots[0].coordinate;
-        expect(Object.keys(testSpotInstance)).toStrictEqual(expectSpotProperty);
         expect((testSpotInstance as any).id).toStrictEqual(expectSpotId);
         expect((testSpotInstance as any).name).toStrictEqual(expectSpotName);
         expect((testSpotInstance as any).coordinate).toStrictEqual(expectSpotCoordinate);
@@ -34,16 +30,19 @@ describe('MapViewState.tsのテスト', () => {
     it('toMapTreeがRawMapを受け取ってMap型の木構造を返す', () => {
         const testMapData: RawMap[] = testMapViewState3.maps;
         const actualMap: Map = toMapTree(testMapData);
+        // rootMapとそのSpotのインスタンスを生成し、お互いに登録
         const expectRawMap = testMapViewState3.maps;
         const expectRootMapData = expectRawMap[0];
         const expectMapInstance: Map = createMapInstance(expectRootMapData);
         const expectSpot: Spot = createSpotInstance(expectRootMapData.spots[0]);
         expectMapInstance.addSpots([expectSpot]);
         expectSpot.setParentMap(expectMapInstance);
+        // 子Mapのインスタンスを生成し親Spotとお互いに登録
         const expectDetailMapData = expectRawMap[1];
         const expectDetailMapInstance = createMapInstance(expectDetailMapData);
         expectSpot.addDetailMaps([expectDetailMapInstance]);
         expectDetailMapInstance.setParentSpot(expectSpot);
+        // 子MapのSpotのインスタンスを生成し親Mapとお互いに登録
         const expectDetailSpot = createSpotInstance(expectDetailMapData.spots[0]);
         expectDetailMapInstance.addSpots([expectDetailSpot]);
         expectDetailSpot.setParentMap(expectDetailMapInstance);
