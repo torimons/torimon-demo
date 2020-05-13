04569d92cc23e432dab1f9d10550c2efc52f2cb8
mirlosu <45028997+mirlosu@users.noreply.github.com>
2020-05-13 17:53:47 +0900

    地図作成コンポーネントの雛形作成
    Co-authored-by: shuto410 <takumi.shuto314@gmail.com>
    Co-authored-by: nishikawa1122 <nishikawa@posl.ait.kyushu-u.ac.jp>

---
+++ src/components/CreationMainView/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="creation-map-view">
+      
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component({
+    components: {
+        
+    },
+})
+export default class CreationMainView extends Vue {
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+html,
+body,
+#creation-main-view {
+  position: relative;
+  height: 100%;
+}
+
+body {
+  margin: 0;
+}
+</style>
