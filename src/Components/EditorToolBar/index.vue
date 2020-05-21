7084b4b43ea576878d8dc06a6456eda607f7640a
mirlosu <45028997+mirlosu@users.noreply.github.com>
2020-05-21 17:18:28 +0900

    floorSwitchButtonコンポーネントを元にeditorToolBarの仮組み
    Co-authored-by: nishikawa1122 <nishikawa@posl.ait.kyushu-u.ac.jp>
    Co-authored-by: shuto410 <takumi.shuto314@gmail.com>

---
+++ src/Components/EditorToolBar/index.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="editor-tool-bar">
+      <v-container class="pa-0">
+            <v-row
+                no-gutters
+            >
+                <v-col>
+                    <v-btn-toggle
+                        tile
+                        mandatory
+                        color="primary"
+                    >
+                        <v-btn>
+                            hoge
+                        </v-btn>
+                        <v-btn>
+                            hoge
+                        </v-btn>
+                        <v-btn>
+                            hoge
+                        </v-btn>
+                    </v-btn-toggle>
+                </v-col>
+            </v-row>
+        </v-container>
+    </div>
+</template>
+
+<script lang="ts" src="index.ts">
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+html,
+body,
+#editor-tool-bar {
+  position: relative;
+  height: 100%;
+}
+
+body {
+  margin: 0;
+}
+</style>
--- src/Components/MainCreationView/index.vue
+++ src/Components/MainCreationView/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="main-creation-view">
-      
+      <EditorToolBar/>
     </div>
 </template>
 
