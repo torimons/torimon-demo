3d2659f5c70f7d14eff265150ccd9f149863669f
ikeda <ikeda@posl.ait.kyushu-u.ac.jp>
2020-05-28 16:16:19 +0900

    SpotItemの修正漏れ
    Co-authored-by: Gen Imamura <igen.kuel@gmail.com>

--- src/components/SpotItem/index.vue
+++ src/components/SpotItem/index.vue
@@ -9,8 +9,8 @@
         >
             <v-list-item two-line>
                 <v-list-item-content>
-                    <v-list-item-title class="headline">{{ parentSpotName + " " + spot.name }}</v-list-item-title>
-                    <v-list-item-subtitle>{{ spot.floorName + " " + distance }}</v-list-item-subtitle>
+                    <v-list-item-title class="headline">{{ parentSpotName + " " + spot.getName() }}</v-list-item-title>
+                    <v-list-item-subtitle>{{ spot.getFloorName() + " " + distance }}</v-list-item-subtitle>
                 </v-list-item-content>
             </v-list-item>
         </v-card>
