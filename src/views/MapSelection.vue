3ba4d373b62215304f4036f856b2deb6552ea78a
imamura-gen <igen.kuel@gmail.com>
2020-06-03 18:26:46 +0900

    インデントの修正

--- src/views/MapSelection.vue
+++ src/views/MapSelection.vue
@@ -1,17 +1,22 @@
 <template>
     <div class="map-selection">
         <v-app>
+            <v-card>
+                <v-card-title>
+                    <h1 class="title">Map Selection Page</h1>
+                </v-card-title>
+            </v-card>
             <v-container id="search-bar" class="pt-0" >
-            <v-row>
-                <v-col
-                cols="12"
-                sm="4"
-                md="3"
-                class="pa-0"
-                >
-                <MapSearch/>
-                </v-col>
-            </v-row>
+                <v-row>
+                    <v-col
+                        cols="12"
+                        sm="4"
+                        md="3"
+                        class="pa-0"
+                    >
+                        <MapSearch/>
+                    </v-col>
+                </v-row>
             </v-container>
         </v-app>
     </div>
@@ -33,8 +38,8 @@ export default class MapSelection extends Vue {
 <style scoped>
 #search-bar {
   position: absolute;
-  left: 0;
-  top: 0;
+  left: 0px;
+  top: 65px;
   z-index: 1000;
   pointer-events: none;
 }
