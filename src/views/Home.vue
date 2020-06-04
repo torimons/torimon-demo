f4a68ce14780449e75d7000711a60e0e6102b237
ikeda <ikeda@posl.ait.kyushu-u.ac.jp>
2020-06-04 15:42:41 +0900

    インデント修正

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,29 +5,29 @@
                 class="fill-height"
                 fluid
             >
-                    <v-row
-                        align="center"
-                        justify="center"
-                    >
-                        <v-col class="text-center">
-                            <div class="">
-                                Torimon
-                            </div>
-                            <div class="">
-                                Map for everyone.
-                            </div>
-                            <div class="my-2">
-                                <v-btn color="#3F8373" large>
-                                    <span class="white--text">地図利用</span>
-                                </v-btn>
-                            </div>
-                            <div class="my-2">
-                                <v-btn color="#3F8373" large>
-                                    <span class="white--text">地図作成</span>
-                                </v-btn>
-                            </div>
-                        </v-col>
-                    </v-row>
+                <v-row
+                    align="center"
+                    justify="center"
+                >
+                    <v-col class="text-center">
+                        <div class="">
+                            Torimon
+                        </div>
+                        <div class="">
+                            Map for everyone.
+                        </div>
+                        <div class="my-2">
+                            <v-btn color="#3F8373" large>
+                                <span class="white--text">地図利用</span>
+                            </v-btn>
+                        </div>
+                        <div class="my-2">
+                            <v-btn color="#3F8373" large>
+                                <span class="white--text">地図作成</span>
+                            </v-btn>
+                        </div>
+                    </v-col>
+                </v-row>
             </v-container>
         </v-content>
     </Menu>
