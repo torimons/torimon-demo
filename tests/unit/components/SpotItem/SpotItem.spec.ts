6797f7b9f1bf29289058cc8a44642d07af14a3dd
imamura-gen <igen.kuel@gmail.com>
2020-06-04 16:01:09 +0900

    warning修正

--- tests/unit/components/SpotItem/SpotItem.spec.ts
+++ tests/unit/components/SpotItem/SpotItem.spec.ts
@@ -39,12 +39,12 @@ describe('components/SpotItem.vue', () => {
         expect(wrapper.find('.v-list-item__title').text()).toBe(testSpot.getName());
         expect(wrapper.find('.v-list-item__subtitle').text()).toBe('1km');
     });
-    
+
     it('対象スポットの親マップに親スポットが存在する場合' , () => {
         // propsに渡すspotにfloorNameをつけると上の親スポットがないテストでも表示されてしまうので別データ用意
         const testSpotWithFloorName: Spot = new Spot(2, 'testSpotWithFloorName', {lat: 0, lng: 0});
         // Map,Spotを相互登録する際に親マップのfloorNameがspotにセットされる
-        const testParentMapWithFloorName: Map = 
+        const testParentMapWithFloorName: Map =
             new Map(3, 'testParentMapWithFloorName', {topL: {lat: 0, lng: 0}, botR: {lat: 0, lng: 0}}, 'testFloor');
         testParentMapWithFloorName.addSpots([testSpotWithFloorName]);
         const parentSpot: Spot = new Spot(4, 'parentSpot', {lat: 0, lng: 0});
@@ -54,7 +54,9 @@ describe('components/SpotItem.vue', () => {
             spot: testSpotWithFloorName,
             distance: '1km',
         });
-        expect(wrapper.find('.v-list-item__title').text()).toBe(parentSpot.getName() + ' ' + testSpotWithFloorName.getName());
-        expect(wrapper.find('.v-list-item__subtitle').text()).toBe(testSpotWithFloorName.getFloorName() + ' ' + '1km');
-    })
+        expect(wrapper.find('.v-list-item__title').text())
+            .toBe(parentSpot.getName() + ' ' + testSpotWithFloorName.getName());
+        expect(wrapper.find('.v-list-item__subtitle').text()).
+            toBe(testSpotWithFloorName.getFloorName() + ' ' + '1km');
+    });
 });
