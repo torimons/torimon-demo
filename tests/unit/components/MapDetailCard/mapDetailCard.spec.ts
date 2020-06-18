bae5097e6120323e298bfe1ad31f3880f0d824da
imamura-gen <igen.kuel@gmail.com>
2020-06-18 18:32:48 +0900

    MapDetailCardの修正

--- tests/unit/components/MapDetailCard/mapDetailCard.spec.ts
+++ tests/unit/components/MapDetailCard/mapDetailCard.spec.ts
@@ -1,6 +1,5 @@
 import { createLocalVue, mount } from '@vue/test-utils';
 import MapDetailCard from '@/components/MapDetailCard/index.vue';
-import router from '@/router';
 import Vuetify from 'vuetify';
 
 
@@ -15,7 +14,6 @@ describe('MapDetailCardコンポーネントのテスト', () => {
         wrapper = mount( MapDetailCard, {
             localVue,
             vuetify,
-            router,
             attachToDocument: true,
         });
     });
@@ -27,10 +25,4 @@ describe('MapDetailCardコンポーネントのテスト', () => {
         expect(wrapper.vm.dialog).toBe(false);
     });
 
-    it('openMapボタンを押すと/MainViewに画面遷移する', () => {
-        wrapper.setData({dialog: true});
-        expect(wrapper.vm.dialog).toBe(true);
-        wrapper.find('.v-dialog').find('.v-btn#openMap').trigger('click');
-        expect(wrapper.vm.$route.path).toBe('/MainView');
-    });
 });
