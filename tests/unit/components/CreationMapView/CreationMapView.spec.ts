fd468fb3129d3f695f8929e3fae8f42d6cab7761
shuto410 <shuto@posl.ait.kyushu-u.ac.jp>
2020-06-22 22:53:15 +0900

    CreationMapViewコンポーネントのテスト追加

--- tests/unit/components/CreationMapView/CreationMapView.spec.ts
+++ tests/unit/components/CreationMapView/CreationMapView.spec.ts
@@ -5,6 +5,9 @@ import { testRawMapData } from '../../../resources/testRawMapData';
 import EditorToolBar from '@/components/EditorToolBar';
 import CreationMapView from '@/components/CreationMapView';
 import Map from '@/Map/Map';
+import Spot from '@/Spot/Spot';
+import SpotMarker from '@/components/MapView/Marker/SpotMarker';
+import L from 'leaflet';
 
 
 describe('components/CreationMapView', () => {
@@ -41,6 +44,67 @@ describe('components/CreationMapView', () => {
         const e = { latlng: { lat: 0, lng: 0 } };
         wrapper.vm.addSpot(e);
         expect(map.getSpots().length).toBe(1);
+    });
+
+    it('switchFocusedMarkerによりフォーカスされるスポットとマーカーが切り替わり，スポットエディターが現れる', () => {
+        const map: Map = wrapper.vm.map;
+        // 切り替え前のスポット用意
+        const oldFocusedSpot = new Spot(0, 'testSpot', { lat: 0, lng: 0 });
+        map.addSpot(oldFocusedSpot);
+        const oldFocusedMarker = new SpotMarker(oldFocusedSpot);
+        wrapper.vm.spotMarkers.push(oldFocusedMarker);
+        wrapper.vm.focusedSpot = oldFocusedSpot;
+        wrapper.vm.spotEditorIsVisible = false;
+
+        // 切り替え後のスポット用意
+        const newFocusedSpot = new Spot(1, 'testSpot', { lat: 0, lng: 0 });
+        map.addSpot(newFocusedSpot);
+        const newFocusedMarker = new SpotMarker(newFocusedSpot);
+
+        wrapper.vm.switchFocusedMarker(newFocusedMarker);
+
+        const actualFocusedSpot: Spot = wrapper.vm.focusedSpot;
+        expect(actualFocusedSpot).toBe(newFocusedSpot);
+
+        const actualSpotEditorIsVisibile: boolean = wrapper.vm.spotEditorIsVisible;
+        expect(actualSpotEditorIsVisibile).toBe(true);
+
+        // 以下アイコンの色切り替わりテスト
+        const actualOldSpotMarkerIcon: L.DivIcon = (oldFocusedMarker as any).getIcon();
+        const actualNewSpotMarkerIcon: L.DivIcon = (newFocusedMarker as any).getIcon();
+        //  期待値用のアイコン作成関数
+        const createIcon = (isSelected: boolean): L.DivIcon => {
+            const normalColor: string = '#3F8373';
+            const selectedColor: string = '#AE56B3';
+            const color = isSelected ? selectedColor : normalColor;
+            const htmlTemplate =
+                `<div class="marker-pin"></div><i class="material-icons" style="font-size:48px; color:${color};">place</i>`;
+            const icon = L.divIcon({
+                className: 'custom-div-icon',
+                html: htmlTemplate,
+                iconAnchor: [24, 50],
+            });
+            return icon;
+        };
+        const expectedNomarlIcon = createIcon(false);
+        const expectedSelectedIcon = createIcon(true);
+        expect(actualOldSpotMarkerIcon).toEqual(expectedNomarlIcon);
+        expect(actualNewSpotMarkerIcon).toEqual(expectedSelectedIcon);
+    });
+
+    it('deleteFocusedMarkerによりfocusedSpotがmapの子スポットから消え，マーカーも地図上から消える', () => {
+        const map: Map = wrapper.vm.map;
+        const testSpot = new Spot(0, 'testSpot', { lat: 0, lng: 0 });
+        map.addSpot(testSpot);
+        const testMarker = new SpotMarker(testSpot);
+        wrapper.vm.spotMarkers.push(testMarker);
+        wrapper.vm.focusedSpot = testSpot;
+        wrapper.vm.deleteFocusedSpot();
+
+        const acutualSpots: Spot[] = map.getSpots();
+        expect(acutualSpots).not.toContain(testSpot);
+        const acutualMarkers: SpotMarker[] = wrapper.vm.spotMarkers;
+        expect(acutualMarkers).not.toContain(testMarker);
     });
 
     it('zoomInによってzoomLevelが大きくなる', () => {
