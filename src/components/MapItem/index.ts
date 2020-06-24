b25d071b7989f042688e47b0eb4e370081f4c658
matsui <matsui@posl.ait.kyushu-u.ac.jp>
2020-06-24 14:12:55 +0900

    dialog画面をMapItemコンポーネントに移動

--- src/components/MapItem/index.ts
+++ src/components/MapItem/index.ts
@@ -1,13 +1,29 @@
-import { Component, Prop, Vue, Emit } from 'vue-property-decorator';
+import { Component, Prop, Vue, Emit, Watch } from 'vue-property-decorator';
+import { mapViewMutations } from '@/store';
+import Map from '@/Map/Map.ts';
 
 @Component
 export default class MapItem extends Vue {
     // 親からマップ名、作成者名を受けとり表示する
-    @Prop() private mapName!: string;
-    @Prop() private description!: string;
+    @Prop() private map!: Map;
+    private name: string = '';
+    private description: string = '';
+    private dialog: boolean = false;
 
-    @Emit('dialog')
-    public openMapDetailCard() {
-        return true;
+    private mounted() {
+        this.name = this.map.getName();
+        this.description = this.map.getDescription() || '';
+    }
+
+    private closeDialog() {
+        this.dialog = false;
+    }
+
+    private openDialog() {
+        this.dialog = true;
+    }
+
+    private openMap() {
+        mapViewMutations.setRootMap(this.map);
     }
 }
