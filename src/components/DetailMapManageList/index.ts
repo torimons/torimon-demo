670dffa832b5d36549bf6e029a6ea77bd1e32b25
matsui <matsui@posl.ait.kyushu-u.ac.jp>
2020-06-27 17:04:53 +0900

    詳細マップ管理用のコンポーネント作成

---
+++ src/components/DetailMapManageList/index.ts
@@ -0,0 +1,51 @@
+import { Component, Vue, Prop } from 'vue-property-decorator';
+import DeleteConfirmationDialog from '@/components/DeleteConfirmationDialog/index.vue';
+import Map from '@/Map/Map';
+
+@Component({
+    components: {
+        DeleteConfirmationDialog,
+    },
+})
+export default class DetailMapManageList extends Vue {
+    @Prop() public detailMaps!: Map[];
+    public dialog: boolean = false;
+    public selectedMap: Map | null = null;
+    public selectedMapName: string = '';
+
+    /**
+     * 複製ボタンが押されると呼び出され、
+     * 指定した詳細マップを複製するイベントを発火する。
+     * @param detailMap 複製対象の詳細マップ
+     */
+    private duplicateMap(detailMap: Map) {
+        this.$emit('dup', detailMap);
+    }
+
+    /**
+     * 削除ボタンを押すと呼び出され、削除確認ダイアログを表示する。
+     * @param detailMap 削除対象の詳細マップ
+     */
+    private confirmMapDeletion(detailMap: Map): void {
+        this.dialog = true;
+        this.selectedMap = detailMap;
+        this.selectedMapName = detailMap.getName();
+    }
+
+    /**
+     * DeleteConfirmationDialogで削除ボタンが押されると呼び出され、
+     * 指定された詳細マップを削除するイベントを発火する。
+     */
+    private deleteMap() {
+        this.dialog = false;
+        this.$emit('del', this.selectedMap!.getId());
+    }
+
+    /**
+     * DeleteConfirmationDialogでCancelボタンが押されると呼び出され、
+     * 削除確認ダイアログを閉じる。
+     */
+    private cancelMapDeletion() {
+        this.dialog = false;
+    }
+}
---
+++ src/components/DetailMapManageList/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-card>
+    <v-list
+      tile
+      dense
+      style="max-height:200px; overflow-y:auto;"
+    >
+      <v-list-item
+        v-for="(detailMap, index) in detailMaps"
+        :key="index"
+      >
+        <v-list-item-content>
+          <v-list-item-title>
+            {{ detailMap.getName() }}
+          </v-list-item-title>
+        </v-list-item-content>
+        <v-btn icon>
+          <v-icon>edit</v-icon>
+        </v-btn>
+        <v-btn
+          icon
+          @click="duplicateMap(detailMap)"
+        >
+          <v-icon>file_copy</v-icon>
+        </v-btn>
+        <v-btn
+          icon
+          @click.stop="confirmMapDeletion(detailMap)"
+        >
+          <v-icon>delete</v-icon>
+        </v-btn>
+      </v-list-item>
+    </v-list>
+    <v-dialog
+      v-model="dialog"
+      width="500"
+    >
+      <delete-confirmation-dialog
+        :name=selectedMapName
+        @del="deleteMap"
+        @cancel="cancelMapDeletion"
+      ></delete-confirmation-dialog>
+    </v-dialog>
+    <v-card-actions>
+      <v-btn
+        block
+        color="#3fa590"
+        outlined
+        @click="$emit('add')"
+      >
+        <v-icon>add</v-icon>
+        add detail map
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script lang="ts" src="./index.ts"/>
+
+<style scoped>
+</style>
