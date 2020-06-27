8ed4c35775f3ba5865a3029770defe0a4a087809
matsui <matsui@posl.ait.kyushu-u.ac.jp>
2020-06-27 17:04:30 +0900

    削除確認ダイアログコンポーネントの作成

---
+++ src/components/DeleteConfirmationDialog/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-card>
+    <v-card-title
+      class="headline"
+      primary-title
+    >
+      {{ name }}を削除しますか？
+    </v-card-title>
+    <v-divider></v-divider>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn
+        text
+        color="primary"
+        @click="confirmedDeletion"
+      >
+        Delete
+      </v-btn>
+      <v-btn
+        color="secondary"
+        text
+        @click="cancelDeletion"
+      >
+        Cancel
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script lang="ts" src="./index.ts"/>
+
+<style scoped>
+</style>
